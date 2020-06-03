ff992333ba589d9c5dffedff8b1b320675d605db
Ethan <ethan-bit@protonmail.com>
2020-06-03 10:40:14 +0800

    Initial Commit

---
+++ interact-OSX/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  interact-OSX
+//
+//  Created by 石金圆 on 2020/6/3.
+//  Copyright © 2020 ethan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
